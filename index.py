08f8fd06c118a8448abf569a5953462523be653d
Anandhbasha <anadhakm@gmail.com>
2025-09-25 18:13:24 +0530

    datatypes

---
+++ index.py
@@ -0,0 +1,29 @@
+'''print("Hello welcome to python",end='\t')
+print("First Page of Python")
+
+print("apple","Banana","Kiwi",sep="-") '''
+# premitive data types
+
+a=10
+print(a)
+
+# int
+
+b=2.0
+b=60
+# print(type(b))
+print(b)
+# float
+# boolean
+isAvlie = True
+print(type(isAvlie))
+
+# string
+x="xyz"
+print(type(x))
+
+# complex data types
+# list
+# tuple
+# set
+# dictionary

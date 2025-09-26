eaae489ad818d21ee26532270b49490912dfdbcf
Anandhbasha <anadhakm@gmail.com>
2025-09-26 18:22:50 +0530

    operators

--- index.py
+++ index.py
@@ -2,28 +2,100 @@
 print("First Page of Python")
 
 print("apple","Banana","Kiwi",sep="-") '''
-# premitive data types
+# # premitive data types
 
-a=10
-print(a)
+# a=10
+# print(a)
 
-# int
+# # int
 
-b=2.0
-b=60
-# print(type(b))
-print(b)
-# float
-# boolean
-isAvlie = True
-print(type(isAvlie))
+# b=2.0
+# b=60
+# # print(type(b))
+# print(b)
+# # float
+# # boolean
+# isAvlie = True
+# print(type(isAvlie))
 
-# string
-x="xyz"
-print(type(x))
+# # string
+# x="xyz"
+# print(type(x))
 
 # complex data types
 # list
+list = [10,20,50,40.0,"xyz",True]
+# print(list)
+# print(list[0])
+# print(list[1])
+# print(list[2])
+# print(list[3])
+# print(list[4])
+# print(list[5])
+# list[0] = 100
+# print(list)
 # tuple
+# userId = (101,102,103,104,105)
+# print(userId(0))
+# userId[0] = 107
+# print(userId[0])
+# print(userId[1])
+# print(userId[2])
+# print(userId[3])
 # set
-# dictionary
+# newsets ={101,101,105,104,102,103,102,103,106,107,108}
+# print(newsets)
+# # dictionary
+# person = {
+#     "name":"amudha",
+#     "age":20,
+#     "address":"CBE",
+#     "family":{
+#         "dad":"hghjgjh",
+#         "mom":"nbjhhjhjh"
+#     }
+# }
+
+# print(person)
+# print(person["family"])
+
+# operators
+# arithmetic
+# +,-,/,//,*,%,**
+a=10
+b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(10%3)
+# print(a**b)
+
+# Comparison
+# >,<,>=,<=,==
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=10)
+# print(a==11)
+# print(a!=10)
+
+# logical
+# and or not
+# print(a<b and a==10 and a>=b)
+# print(a<b or a!=10 or a>=b)
+# print(not a!=10)
+
+# membership 
+list = ["apple","banana","kiwi"]
+print("apple" in list)
+print("Apple" not in list)
+
+# idendity
+list1 =[1,2,3]
+list2 = [1,2,3]
+s= list1
+
+print(list1 is list2)
+print(list1 is s)

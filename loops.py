b8ce4bf460fb83e965b1d0dfe825e2837f6e28c2
Anandhbasha <anadhakm@gmail.com>
2025-09-30 18:36:28 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,59 @@
+# second = 5
+
+# while second>0:
+#     # print("Time left:"+second)
+#     print(f'Time left:{second}')
+#     # second = second-1
+#     second-=1
+
+# list = [44,55,66,77,88]
+# print(list[0])
+# print(list[1])
+# print(list[2])
+# print(list[3])
+# print(list[4])
+
+# a=0
+# list = [44,55,66,77,88]
+# # while a<len(list):
+# #     print(list[a])
+# #     a+=1
+
+# # for
+# for b in list:
+#     print(b)
+
+# range
+# for x in range(10):
+#     print(x)
+
+# for x in range(1,11):
+#     print(x)
+
+# for x in range(0,10,3):
+#     print(x)
+
+# fruits = ["apple","banana","cherry"]
+
+# fruitName = str(input("Enter the fruit Name:")).lower()
+
+# for fruit in fruits:
+#     if fruitName==fruit:
+#         print(f'You are selected:{fruit}')
+
+list1 = (1,13,33,8,22,44,17,21,91)
+for a in list1:
+   if a%2!=0:
+      print(a)
+
+prices = [410,220,812]
+finalPrice = [p+(p*0.18) for p in prices]
+print(finalPrice)
+
+# reverse
+names = "xyz" #names[0]=x,names[1]=y,names[2]=z
+# output zyx
+
+# x
+# xy
+# xyz

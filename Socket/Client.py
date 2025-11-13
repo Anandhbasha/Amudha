48af0669f5b3b336d7a4caf55c822b1f680849ab
Anandhbasha <anadhakm@gmail.com>
2025-11-13 18:06:45 +0530

    socket

---
+++ Socket/Client.py
@@ -0,0 +1,15 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+client_socket.connect(("127.0.0.1",9000))
+
+client_socket.send("Hello Server".encode())
+
+# receive data from server
+
+data = client_socket.recv(1024).decode()
+
+print("server says:",data)
+
+client_socket.close()
